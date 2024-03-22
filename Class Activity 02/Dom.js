d0451fd710ba947a337283feb7dc10328955f404
AnumKhashr <163669446+AnumKhashr@users.noreply.github.com>
2024-03-22 15:46:24 +0500

    Delete Class Activity 02 directory

--- Class Activity 02/Dom.js
+++
@@ -1,7 +0,0 @@
-function update(){
-    console.log("Update Results");
-    var input=document.getElementById('myinput');
-    var output=document.getElementById('output');
-    output.innerHTML=input.value;
-    console.log(input.value);
-    }
