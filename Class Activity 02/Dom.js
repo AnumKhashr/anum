51fb9bb23b61a236a1564ba8b8b0673c74788095
AnumKhashr <163669446+AnumKhashr@users.noreply.github.com>
2024-03-22 14:01:40 +0500

    Add files via upload

---
+++ Class Activity 02/Dom.js
@@ -0,0 +1,7 @@
+function update(){
+    console.log("Update Results");
+    var input=document.getElementById('myinput');
+    var output=document.getElementById('output');
+    output.innerHTML=input.value;
+    console.log(input.value);
+    }
